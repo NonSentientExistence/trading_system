2ba1cf8dbcddebd58f693729ec0c620c11b7dc48
NonSentientExistence <david@venngren.se>
2025-09-30 00:59:08 +0200

    Coded login function(not validating correctly), added menus for logged in user in main. Added class for item creation. Added static user list.
    To validate user login data. Menu options for future functionality.
    Class for items to be traded. Static list for testing login function.

---
+++ Item.cs
@@ -0,0 +1,20 @@
+namespace App;
+
+//class for item creation
+
+public class Item
+{
+  string Name;
+  string Description;
+  string Owner;
+
+
+  public Item(string name, string owner, string description)
+  {
+    Name = name;
+    Owner = owner;
+    Description = description;
+
+  }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,11 +1,16 @@
 ﻿using App;
 
+List<IUser> users = new List<IUser>();
+
+users.Add(new User("User 1", "user1@e", "pass"));
+users.Add(new User("User 2", "user2@e", "pass"));
+
 //create variable for while loop
 bool is_running = true;
 
-// create and set the variable for which menu to use
-
+// define and set the variable for which menu to use and user_logged_in to false default.
 EMenu menu_choice = EMenu.Main;
+IUser? user_logged_in = null;
 
 //while loop for menu
 
@@ -20,13 +25,55 @@ while (is_running)
       {
         Console.WriteLine("Welcome to the main menu!");
         Console.ReadLine();
+
+        if (user_logged_in == null)
+        {
+          menu_choice = EMenu.Login;
+        }
+        if (user_logged_in != null)
+        {
+          //Write line for menu choices on main menu
+          Console.WriteLine("Menu: \n\n1.Check Inventory \n2. New trade \n3. Pending trades \n4. Trade history");
+
+        }
+
       }
       break;
+    // Login menu, if user not logged in then menu_choice will be set to EMenu.Login
+    case EMenu.Login:
+      {
+        //Check if user is logged in
+        if (user_logged_in == null)
+        {
+          Console.WriteLine("Please enter your email address and password!");
+          Console.WriteLine("Email: ");
+          string user_email = Console.ReadLine();
+          Console.WriteLine("Password: ");
+          string user_password = Console.ReadLine();
 
+          //loops available users, matching email and password.
+          foreach (IUser user in users)
+          {
+            if (user.TryLogin(user_email, user_password))
+            {
+              user_logged_in = user;
+              break;
+            }
+          }
+        }
+        if (user_logged_in != null)
+        {
+          menu_choice = EMenu.Main;
+        }
+
+      }
+
+      break;
     //Hidden test menu
     case EMenu.Test:
       {
         Console.WriteLine("Sooooo, you need to test stuff?");
+
       }
       break;
     //Exit menu

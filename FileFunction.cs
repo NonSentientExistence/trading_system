d7d3fc678d17cd8e152032d58409e7504d41f4ee
NonSentientExistence <david@venngren.se>
2025-10-03 21:19:24 +0200

    Cleaned code in Filefunction. Prev dir check in all if write cases. Just moved to one in start if func. Removed use of path variable and use sys func directly in file append.
    Cleaner and easier to read.

--- FileFunction.cs
+++ FileFunction.cs
@@ -8,45 +8,32 @@ public class FileFunction
   //Function for write to file
   public static void FileWrite(string data, string type)
   {
-    string filePath;
+
+    //Check if the Data folder exists, otherwise create it
+    if (!Path.Exists("Data"))
+    {
+      Directory.CreateDirectory("Data");
+    }
 
     //Sets file path depending on which data is sent to FileWrite
     if (type == "u")
     {
-      //Check if the Data folder exists, otherwise create it
-      if (!Path.Exists("Data"))
-      {
-        Directory.CreateDirectory("Data");
-      }
       //Writes data to file
       File.AppendAllText(Path.Combine("Data", "users.txt"), data);
-
     }
     if (type == "i")
     {
-      filePath = Path.Combine("Data", "items.txt");
-      //Check if the Data folder exists, otherwise create it
-      if (!Path.Exists("Data"))
-      {
-        Directory.CreateDirectory("Data");
-      }
       //Writes data to file
       File.AppendAllText(Path.Combine("Data", "items.txt"), data);
 
     }
     if (type == "l")
     {
-      filePath = Path.Combine("Data", "log.txt");
-      //Check if the Data folder exists, otherwise create it
-      if (!Path.Exists("Data"))
-      {
-        Directory.CreateDirectory("Data");
-      }
       File.AppendAllText(Path.Combine("Data", "log.txt"), data);
-
     }
-
-
-
   }
+
+  
+
+
 }
